75b002aec07f6f4fdcd9a4d29a60d822256c8d32
Tbetcha <troy.boettger@gmail.com>
2020-06-10 00:52:52 -0400

    set a loading state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import axios from 'axios'
 export default function App() {
 	const [results, setResults] = useState([])
 	const [query, setQuery] = useState('react hooks')
+	const [loading, setLoading] = useState(false)
+	const searchInputRef = useRef()
 
 	useEffect(() => {
 		getResults()
@@ -17,29 +19,48 @@ export default function App() {
 	}, [query])
 
 	const getResults = async () => {
+		setLoading(true)
 		const response = await axios.get(
 			`http://hn.algolia.com/api/v1/search_by_date?query=${query}`
 		)
 		setResults(response.data.hits)
+		setLoading(false)
 	}
 	const handleSubmit = event => {
 		event.preventDefault()
 		getResults()
 	}
 
+	const handleClear = () => {
+		setQuery('')
+		searchInputRef.current.focus()
+	}
+
 	return (
 		<>
 			<form onSubmit={handleSubmit}>
-				<input type='text' value={query} onChange={event => setQuery(event.target.value)} />
+				<input
+					type='text'
+					value={query}
+					onChange={event => setQuery(event.target.value)}
+					ref={searchInputRef}
+				/>
 				<button type='submit'>Search</button>
+				<button type='button' onClick={handleClear}>
+					Clear
+				</button>
 			</form>
-			<ul>
-				{results.map(result => (
-					<li key={result.objectID}>
-						<a href={result.url}>{result.title}</a>
-					</li>
-				))}
-			</ul>
+			{loading ? (
+				<div> Loading Results </div>
+			) : (
+				<ul>
+					{results.map(result => (
+						<li key={result.objectID}>
+							<a href={result.url}>{result.title}</a>
+						</li>
+					))}
+				</ul>
+			)}
 		</>
 	)
 }
