9eb4720d14d9527813e9201570aed200d6625ceb
Tbetcha <troy.boettger@gmail.com>
2020-06-10 01:23:54 -0400

    add stylinh and complete network error state

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
 	const [results, setResults] = useState([])
 	const [query, setQuery] = useState('react hooks')
 	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState(null)
 	const searchInputRef = useRef()
 
 	useEffect(() => {
@@ -20,12 +21,17 @@ export default function App() {
 
 	const getResults = async () => {
 		setLoading(true)
-		const response = await axios.get(
-			`http://hn.algolia.com/api/v1/search_by_date?query=${query}`
-		)
-		setResults(response.data.hits)
+		try {
+			const response = await axios.get(
+				`http://hn.algolia.com/api/v1/search_by_date?query=${query}`
+			)
+			setResults(response.data.hits)
+		} catch (err) {
+			setError(err)
+		}
 		setLoading(false)
 	}
+
 	const handleSubmit = event => {
 		event.preventDefault()
 		getResults()
@@ -37,30 +43,36 @@ export default function App() {
 	}
 
 	return (
-		<>
-			<form onSubmit={handleSubmit}>
+		<div className='container max-w-md mx-auto p-4 m-2 bg-purple-lightest shadow-lg rounded'>
+			<img src='https://icon.now.sh/react/c0c' alt='React Logo' className='float-right h-12' />
+			<h1 className='text-grey-darkest font-thin'>Hooks News</h1>
+			<form onSubmit={handleSubmit} className='mb-2'>
 				<input
 					type='text'
 					value={query}
 					onChange={event => setQuery(event.target.value)}
 					ref={searchInputRef}
+					className='border p-1 rounded'
 				/>
-				<button type='submit'>Search</button>
-				<button type='button' onClick={handleClear}>
+				<button type='submit' className='bg-orange rounded m-1 p-1'>
+					Search
+				</button>
+				<button type='button' className='bg-teal text-white rounded p-1' onClick={handleClear}>
 					Clear
 				</button>
 			</form>
 			{loading ? (
-				<div> Loading Results </div>
+				<div className='font-bold text-orange-dark'> Loading Results </div>
 			) : (
-				<ul>
+				<ul className='list-reset leading-normal'>
 					{results.map(result => (
-						<li key={result.objectID}>
-							<a href={result.url}>{result.title}</a>
+						<li key={result.objectID} className='text-indigo-dark hover:text-indigo-darkest'>
+							<a href={result.url}>-{result.title} </a>
 						</li>
 					))}
 				</ul>
 			)}
-		</>
+			{error && <div className=' text-red font-bold'>{error.message}</div>}
+		</div>
 	)
 }
--- src/index.js
+++ src/index.js
@@ -12,7 +12,11 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById('root')
 )
+//set hot reloading
 
+if (module.hot) {
+	module.hot.accept()
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
